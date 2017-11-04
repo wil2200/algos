1557c674042123e85809c0ae9cf4fe8ffdd078b9
wil2200 <wil2200@yahoo.com>
2017-11-04 18:29:14 -0400

    union find (quadratic) and quick find (trees)

---
+++ unionFind/unionFind/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Policy;
+
+namespace unionFind
+{
+    internal class Program
+    {
+        static int n = 40;
+
+        private static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+            sw.Start();
+            RunUnion1();
+            sw.Stop();
+
+
+         
+
+            sw2.Start();
+            RunUnion2();
+            sw2.Stop();
+
+            Console.WriteLine($"Union1 took {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"Union2 took {sw2.ElapsedMilliseconds}");
+
+            Console.ReadLine();
+        }
+
+        public static void RunUnion1()
+        {
+            var uf = new UnionFind(n);
+
+            uf.GenerateRandomPair(n);
+            uf.PrintPairs();
+
+            foreach (var pair in uf.Pairs)
+            {
+                if (uf.IsConnected(pair.Item1, pair.Item2))
+                {
+                    Console.WriteLine($"Already connected {pair.Item1}, {pair.Item2}");
+                    continue;
+                }
+
+                Console.WriteLine($"Unioning pair ({pair.Item1},{pair.Item2})");
+                uf.Union(pair.Item1, pair.Item2);
+                Console.WriteLine($"Now have {uf.ComponentCount()} components");
+
+                uf.PrintSiteInfo();
+
+            }
+        }
+
+
+        public static void RunUnion2()
+        {
+            var uf = new UnionFind(n);
+
+            uf.GenerateRandomPair(n);
+            uf.PrintPairs();
+
+            foreach (var pair in uf.Pairs)
+            {
+                if (uf.IsConnected(pair.Item1, pair.Item2))
+                {
+                    Console.WriteLine($"Already connected {pair.Item1}, {pair.Item2}");
+                    continue;
+                }
+
+                Console.WriteLine($"Unioning pair ({pair.Item1},{pair.Item2})");
+                uf.Union2(pair.Item1, pair.Item2);
+                Console.WriteLine($"Now have {uf.ComponentCount()} components");
+
+                uf.PrintSiteInfo();
+            }
+        }
+    }
+}
---
+++ unionFind/unionFind/UnionFind.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unionFind
+{
+    public class UnionFind
+    {
+        private readonly Random rnd;
+        private readonly int sampleSize;
+        private readonly int[] siteIds;
+        private int component;
+
+        public UnionFind(int sampleSize)
+        {
+            this.sampleSize = sampleSize;
+            component = sampleSize;
+            siteIds = new int [sampleSize];
+            rnd = new Random();
+            Pairs = new List<Tuple<int, int>>();
+            GenerateSites();
+        }
+
+        public List<Tuple<int, int>> Pairs { get; }
+
+        public int ComponentCount()
+        {
+            return component;
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public void PrintSiteInfo()
+        {
+            for (var i = 0; i < siteIds.Length; i++)
+                Console.WriteLine($"Site = {i}, Value = {siteIds[i]}");
+        }
+
+        private int Find(int x)
+        {
+            return siteIds[x];
+        }
+
+        private int Find2(int x)
+        {
+            while (x != siteIds[x])
+                x = siteIds[x];
+            return x;
+        }
+
+        public void Union2(int p, int q)
+        {
+            // to put in same component, give p and q same root
+            var proot = Find2(p);
+            var qroot = Find2(q);
+
+            if (proot == qroot) return;
+
+            siteIds[proot] = qroot;
+            component--;
+        }
+
+
+        public void Union(int p, int q)
+        {
+            var pid = Find(p);
+            var qid = Find(q);
+
+            if (pid == qid) return;
+
+            for (var i = 0; i < siteIds.Length; i++)
+                // rename pIds to qIds to 'connect' them
+                if (siteIds[i] == pid) siteIds[i] = qid;
+
+            component--;
+        }
+
+        private void GenerateSites()
+        {
+            for (var i = 0; i < sampleSize; i++)
+            {
+                siteIds[i] = i;
+                Console.WriteLine($"i - {i}  siteId - {siteIds[i]}");
+            }
+        }
+
+        public void GenerateRandomPair(int max)
+        {
+            for (var i = 0; i < max; i++)
+            {
+                var left = rnd.Next(max - 1);
+                var right = rnd.Next(max - 1);
+
+                Pairs.Add(new Tuple<int, int>(left, right));
+            }
+        }
+
+        public void PrintPairs()
+        {
+            foreach (var tuple in Pairs)
+                Console.WriteLine($"{tuple.Item1} {tuple.Item2}");
+        }
+    }
+}
